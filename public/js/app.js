4a20980ba39187a0bffc00852a89bcde0a8edaed
Tuan Phung <tuan@phung.cz>
2016-07-19 01:00:00 +0200

    new changes

--- public/js/app.js
+++ public/js/app.js
@@ -4,18 +4,28 @@ var socket = io();
 
 console.log(name + ' wants to join ' + room)
 
+$('.room-title').text(room);
+
 socket.on('connect', function () {
-  console.log('yaaaa, connected to sockets')
+  console.log('yaaaa, connected to sockets');
+
+  socket.emit('joinRoom', {
+    name: name,
+    room: room
+  });
 });
 
 socket.on('message', function (message) {
-  var momentTimeStamp = moment.utc(message.momentTimeStamp);
-  var $message =  $('.messages')
+  var momentTimeStamp = moment.utc(message.timestamp);
+  var $messages =  $('.messages');
+  var $message = $('<li class="list-group-item"></li>')
+
   console.log('new message: ');
   console.log(message.text);
 
 $message.append('<p><strong>' + message.name + ' ' + momentTimeStamp.local().format('h:mm')  + '<strong></p>')
-$message.append('<p>' + message.text  + '</p>')
+$message.append('<p>' + message.text  + '</p>');
+$messages.append($message);
 });
 
 // Handles submitting of new message
--- server.js
+++ server.js
@@ -6,23 +6,77 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
+var clientInfo = {};
+
+// sends current users to provided socket
+function sendCurrentUsers(socket) {
+  var info = clientInfo[socket.id];
+  var users = [];
+
+  if (typeof info === 'undefiend') {
+    return;
+  }
+  Object.keys(clientInfo).forEach(function (socketId) {
+    var userInfo = clientInfo[socketId];
+    if (info.room === userInfo.room) {
+      users.push(userInfo.name);
+    }
+  });
+  socket.emit('message', {
+    name: 'System',
+    text: 'Current users: ' + users.join(', '),
+    timestamp: moment().valueOf()
+  })
+}
 
 io.on('connection', function (socket) {
   console.log('user connected via socket.io');
- 
- socket.on('message', function(message){
-   console.log('message recieved: ' + message.text);
-   message.timeStamp = moment().valueOf();
-   // socket.broadcast.emit send to everyone except the sender
-    // socket.emit we have to use io.emit
-   io.emit('message', message);
- })
 
- // this prints out message to console
+  socket.on('disconnect', function () {
+    var userData = clientInfo[socket.id];
+
+    if (typeof userData !== 'undefined') {
+      socket.leave(userData.room);
+      io.to(userData.room).emit('message', {
+        name: 'System',
+        text: userData.name + ' has left!',
+        timestamp: moment().valueOf()
+      });
+      delete clientInfo[socket.id];
+    }
+  });
+
+  socket.on('joinRoom', function (req) {
+    clientInfo[socket.id] = req;
+    socket.join(req.room);
+    socket.broadcast.to(req.room).emit('message', {
+      name: 'System',
+      text: req.name + ' has joined!',
+      timestamp: moment().valueOf()
+    })
+  });
+
+  socket.on('message', function (message) {
+    console.log('message recieved: ' + message.text);
+
+
+    if (message.text === '@currentUsers') {
+      sendCurrentUsers(socket);
+    } else {
+      message.timestamp = moment().valueOf();
+      // socket.broadcast.emit send to everyone except the sender
+      // socket.emit we have to use io.emit
+      io.to(clientInfo[socket.id].room).emit('message', message);
+    }
+
+
+  })
+
+  // this prints out message to console
   socket.emit('message', {
-    name: 'System', 
+    name: 'System',
     text: 'Welcome to TuanoChatt',
-      timeStamp: moment().valueOf()
+    timestamp: moment().valueOf()
   });
 });
 
